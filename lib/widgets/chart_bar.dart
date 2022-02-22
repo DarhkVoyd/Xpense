d2ca0847e80db2d459bd18c2e4a492f3cb9d2ccb
D V <divyadityasnaruka@gmail.com>
2022-02-22 21:20:18 +0530

    add Chart, Better UI

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('\u{20B9}${spendingAmount.toStringAsFixed(0)}'),
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 100,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(20),
+              )),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
